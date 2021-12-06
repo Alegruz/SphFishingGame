9fdbcc1284abfed6e030c070fb31b92fe4fa4c4b
DESKTOP-0FNNCB7\jinju <alegruz@khu.ac.kr>
2021-12-06 11:32:34 +0900

    [NEW] added CUDA Library

---
+++ CudaLibrary/CudaLibrary/kernel.cu
@@ -0,0 +1,327 @@
+﻿#include <cstdlib>
+#include <cstdio>
+#include <string.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "helper_cuda.h"
+#include "helper_functions.h"
+
+#include "thrust/device_ptr.h"
+#include "thrust/for_each.h"
+#include "thrust/iterator/zip_iterator.h"
+#include "thrust/sort.h"
+
+#include "particles_kernel_impl.cuh"
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+int main()
+{
+    const int arraySize = 5;
+    const int a[arraySize] = { 1, 2, 3, 4, 5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+        c[0], c[1], c[2], c[3], c[4]);
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+{
+    int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy input vectors from host memory to GPU buffers.
+    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
+    return cudaStatus;
+}
+
+
+//particleSystem_cuda.cu
+extern "C"
+{
+
+    void cudaInit(int argc, char** argv)
+    {
+        int devID;
+
+        // use command-line specified CUDA device, otherwise use device with highest Gflops/s
+        devID = findCudaDevice(argc, (const char**)argv);
+
+        if (devID < 0)
+        {
+            printf("No CUDA Capable devices found, exiting...\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    void allocateArray(void** devPtr, size_t size)
+    {
+        checkCudaErrors(cudaMalloc(devPtr, size));
+    }
+
+    void freeArray(void* devPtr)
+    {
+        checkCudaErrors(cudaFree(devPtr));
+    }
+
+    void threadSync()
+    {
+        checkCudaErrors(cudaDeviceSynchronize());
+    }
+
+    void copyArrayToDevice(void* device, const void* host, int offset, int size)
+    {
+        checkCudaErrors(cudaMemcpy((char*)device + offset, host, size, cudaMemcpyHostToDevice));
+    }
+
+    void setParameters(SimParams* hostParams)
+    {
+        // copy parameters to constant memory
+        checkCudaErrors(cudaMemcpyToSymbol(params, hostParams, sizeof(SimParams)));
+    }
+
+    //Round a / b to nearest higher integer value
+    uint iDivUp(uint a, uint b)
+    {
+        return (a % b != 0) ? (a / b + 1) : (a / b);
+    }
+
+
+    // compute grid and thread block size for a given number of elements
+    void computeGridSize(uint n, uint blockSize, uint& numBlocks, uint& numThreads)
+    {
+        numThreads = min(blockSize, n);
+        numBlocks = iDivUp(n, numThreads);
+    }
+
+
+
+    void integrateSystem(float* pos,
+        float* vel,
+        float deltaTime,
+        uint numParticles)
+    {
+        thrust::device_ptr<float4> d_pos4((float4*)pos);
+        thrust::device_ptr<float4> d_vel4((float4*)vel);
+
+        thrust::for_each(
+            thrust::make_zip_iterator(thrust::make_tuple(d_pos4, d_vel4)),
+            thrust::make_zip_iterator(thrust::make_tuple(d_pos4 + numParticles, d_vel4 + numParticles)),
+            integrate_functor(deltaTime));
+    }
+
+    void calcHash(uint* gridParticleHash,
+        uint* gridParticleIndex,
+        float* pos,
+        int    numParticles)
+    {
+        uint numThreads, numBlocks;
+        computeGridSize(numParticles, 256, numBlocks, numThreads);
+
+        // execute the kernel
+        calcHashD << < numBlocks, numThreads >> > (gridParticleHash,
+            gridParticleIndex,
+            (float4*)pos,
+            numParticles);
+
+        // check if kernel invocation generated an error
+        getLastCudaError("Kernel execution failed");
+    }
+
+    void reorderDataAndFindCellStart(uint* cellStart,
+        uint* cellEnd,
+        float* sortedPos,
+        float* sortedVel,
+        uint* gridParticleHash,
+        uint* gridParticleIndex,
+        float* oldPos,
+        float* oldVel,
+        uint   numParticles,
+        uint   numCells)
+    {
+        uint numThreads, numBlocks;
+        computeGridSize(numParticles, 256, numBlocks, numThreads);
+
+        // set all cells to empty
+        checkCudaErrors(cudaMemset(cellStart, 0xffffffff, numCells * sizeof(uint)));
+
+        uint smemSize = sizeof(uint) * (numThreads + 1);
+        reorderDataAndFindCellStartD << < numBlocks, numThreads, smemSize >> > (
+            cellStart,
+            cellEnd,
+            (float4*)sortedPos,
+            (float4*)sortedVel,
+            gridParticleHash,
+            gridParticleIndex,
+            (float4*)oldPos,
+            (float4*)oldVel,
+            numParticles);
+        getLastCudaError("Kernel execution failed: reorderDataAndFindCellStartD");
+
+    }
+
+    void computeDensityAndPressure(float* newDensity, float* newPressure,
+        float* sortedPos,
+        uint* gridParticleIndex,
+        uint* cellStart,
+        uint* cellEnd,
+        uint   numParticles,
+        uint   numCells)
+    {
+
+        // thread per particle
+        uint numThreads, numBlocks;
+        computeGridSize(numParticles, 64, numBlocks, numThreads);
+
+        // execute the kernel
+        computeDensityAndPressureDevice << < numBlocks, numThreads >> > (newDensity, newPressure,
+            (float4*)sortedPos,
+            gridParticleIndex,
+            cellStart,
+            cellEnd,
+            numParticles);
+
+        // check if kernel invocation generated an error
+        getLastCudaError("Kernel execution failed");
+
+    }
+
+    void computeForces(float* newVelocities,
+        float* newForce,
+        float deltaTime,
+        float* oldPos,               // input: sorted positions
+        float* oldVel,
+        float* densities,
+        float* pressures,
+        uint* gridParticleIndex,    // input: sorted particle indices
+        uint* cellStart,
+        uint* cellEnd,
+        uint    numParticles,
+        uint   numCells)
+    {
+        // thread per particle
+        uint numThreads, numBlocks;
+        computeGridSize(numParticles, 64, numBlocks, numThreads);
+
+        // execute the kernel
+        computeForceDevice <<< numBlocks, numThreads >>> ((float4*)newVelocities,
+            (float4*)newForce,
+            deltaTime,
+            (float4*)oldPos,               // input: sorted positions
+            (float4*)oldVel,
+            densities,
+            pressures,
+            gridParticleIndex,    // input: sorted particle indices
+            cellStart,
+            cellEnd,
+            numParticles);
+
+        // check if kernel invocation generated an error
+        getLastCudaError("Kernel execution failed");
+    }
+
+
+    void sortParticles(uint* dGridParticleHash, uint* dGridParticleIndex, uint numParticles)
+    {
+        thrust::sort_by_key(thrust::device_ptr<uint>(dGridParticleHash),
+            thrust::device_ptr<uint>(dGridParticleHash + numParticles),
+            thrust::device_ptr<uint>(dGridParticleIndex));
+    }
+
+}   // extern "C"
